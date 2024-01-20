e60d19848f12777190081d558d89d14858a3d861
Betulioo <luis.nunez@bootcamp-upgrade.com>
2024-01-20 14:47:09 +0100

    username required

--- src/api/models/user.model.js
+++ src/api/models/user.model.js
@@ -4,9 +4,9 @@ const Schema = mongoose.Schema;
 
 
 const userSchema = new Schema({
-    email: { type: String, required: true },
+    email: { type: String },
     password: { type: String, required: true },
-    username: { type: String, default: "explorer" },
+    username: { type: String, default: "explorer", required: true },
     role: { type: String, default: "explorer", enum: ["admin", "explorer"] },
     collection: { type: Schema.ObjectId, ref: "Collection" }
 
